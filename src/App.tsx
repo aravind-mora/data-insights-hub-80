8d1913a4b89a35568cbe6f6a6c54d43c29c5c4fc
aravind <aravindmora51@gmail.com>
2026-01-09 10:22:37 +0530

    changes app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -9,33 +8,27 @@ import NotFound from "./pages/NotFound";
 // ✅ import background image
 import background from "./assets/background.jpg";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
 
-      {/* 🌌 Global Background Wrapper */}
-      <div
-  className="min-h-[100vh] bg-fixed bg-cover bg-center"
-  style={{ backgroundImage: `url(${background})` }}
->
-
-        {/* 🌑 Dark transparent overlay */}
-        <div className="min-h-screen bg-black/70">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
+    {/* 🌌 Global Background Wrapper */}
+    <div
+      className="min-h-[100vh] bg-fixed bg-cover bg-center"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      {/* 🌑 Dark transparent overlay */}
+      <div className="min-h-[100vh] bg-black/70">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-    </TooltipProvider>
-  </QueryClientProvider>
+    </div>
+  </TooltipProvider>
 );
 
 export default App;
